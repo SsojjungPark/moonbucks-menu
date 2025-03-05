80ce79262f7375db47d86b50bf4bd2e96cf245ab
Sojung Park <sojung6458@gmail.com>
2025-03-05 12:44:56 +0900

    ♻️ [REFACTOR] step2 리팩토링

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,5 @@
-// Step2 요구사항
-// TODO: localStorage Read & Write
-// - [x] localStorage에 데이터를 저장한다.
-//  - [X] 메뉴를 추가할 때
-//  - [x] 메뉴를 수정할 때
-//  - [x] 메뉴를 삭제할 때
-// - [x] localStorage에 있는 데이터를 읽어온다.
-
-// TODO: 카테고리별 메뉴판 관리
-// - [x] 에스프레소 메뉴판 관리
-// - [x] 프라푸치노 메뉴판 관리
-// - [x] 블렌디드 메뉴판 관리
-// - [x] 티바나 메뉴판 관리
-// - [x] 디저트 메뉴판 관리
-
-// TODO: 페이지 접근시 최초 데이터 Read & Rendring
-// - [x] 페이지에 최초로 로딩될때 localStorage에서 에스프레소 메뉴를 읽어온다.
-// - [x] 에스프레소 메뉴를 페이지에 렌더링한다.
-
-// TODO: 품절
-// - [x] 품절 버튼 추가
-// - [x] sold-out class를 추가하여 상태를 변경한다.
-// - [x] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
-// - [x] 클릭이벤트에서 가장 가까운 li 태그의 class 속성에 sold-out을 추간한다.
-
-// HTML로 부터 태그를 가져올 때 관용적으로 '$'기호 사용
-const $ = selector => document.querySelector(selector);
-
-const store = {
-  setLocalStorage(menu) {
-    localStorage.setItem("menu", JSON.stringify(menu));
-  },
-  getLocalStorage() {
-    return JSON.parse(localStorage.getItem("menu"));
-  },
-};
+import { $ } from "./utils/dom.js";
+import store from "./store/index.js";
 
 function App() {
   const form = $("#menu-form");
@@ -59,6 +25,7 @@ function App() {
       this.menu = store.getLocalStorage();
     }
     render();
+    initEventListener();
   };
 
   const render = () => {
@@ -97,30 +64,26 @@ function App() {
   };
 
   const updateMenuCount = () => {
-    const menuCount = ul.querySelectorAll("li").length;
+    const menuCount = this.menu[this.currentCategory].length;
     menuCountSpan.innerText = `총 ${menuCount}개`;
   };
 
   const addMenuName = () => {
-    const MenuName = input.value;
+    const menuName = input.value;
 
+    if (menuName === "") {
+      alert("에스프레소 메뉴 이름을 입력해주세요.");
+    }
     this.menu[this.currentCategory].push({
-      name: MenuName,
+      name: menuName,
     });
     store.setLocalStorage(this.menu);
     render();
     input.value = "";
-
-    if (MenuName === "") {
-      alert("에스프레소 메뉴 이름을 입력해주세요.");
-    }
   };
 
   const updateMenuName = e => {
     const { menuId } = e.target.closest("li").dataset;
-    // 비구조화 할당 (Destructuring Assignment)
-    // const menuId = e.target.closest("li").dataset.menuId;와 같음
-
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt(
       "메뉴명을 수정해주세요.",
@@ -128,7 +91,7 @@ function App() {
     );
     this.menu[this.currentCategory][menuId].name = updatedMenuName;
     store.setLocalStorage(this.menu);
-    $menuName.innerText = updatedMenuName;
+    render();
   };
 
   const removeMenuName = e => {
@@ -136,8 +99,7 @@ function App() {
       const { menuId } = e.target.closest("li").dataset;
       this.menu[this.currentCategory].splice(menuId, 1);
       store.setLocalStorage(this.menu);
-      e.target.closest("li").remove();
-      updateMenuCount();
+      render();
     }
   };
 
@@ -149,42 +111,44 @@ function App() {
     render();
   };
 
-  ul.addEventListener("click", e => {
-    if (e.target.classList.contains("menu-edit-button")) {
-      updateMenuName(e);
-      return;
-    }
+  const initEventListener = () => {
+    ul.addEventListener("click", e => {
+      if (e.target.classList.contains("menu-edit-button")) {
+        updateMenuName(e);
+        return;
+      }
 
-    if (e.target.classList.contains("menu-remove-button")) {
-      removeMenuName(e);
-      return;
-    }
+      if (e.target.classList.contains("menu-remove-button")) {
+        removeMenuName(e);
+        return;
+      }
 
-    if (e.target.classList.contains("menu-sold-out-button")) {
-      soldOutMenu(e);
-    }
-  });
+      if (e.target.classList.contains("menu-sold-out-button")) {
+        soldOutMenu(e);
+      }
+    });
 
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-  });
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+    });
 
-  submitBtn.addEventListener("click", addMenuName);
+    submitBtn.addEventListener("click", addMenuName);
 
-  input.addEventListener("keypress", e => {
-    if (e.key !== "Enter") return;
-    addMenuName();
-  });
+    input.addEventListener("keypress", e => {
+      if (e.key !== "Enter") return;
+      addMenuName();
+    });
 
-  nav.addEventListener("click", e => {
-    const isCategoryBtn = e.target.classList.contains("cafe-category-name");
-    if (isCategoryBtn) {
-      const { categoryName } = e.target.dataset;
-      this.currentCategory = categoryName;
-      $("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
-      render();
-    }
-  });
+    nav.addEventListener("click", e => {
+      const isCategoryBtn = e.target.classList.contains("cafe-category-name");
+      if (isCategoryBtn) {
+        const { categoryName } = e.target.dataset;
+        this.currentCategory = categoryName;
+        $("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
+        render();
+      }
+    });
+  };
 }
 
 const app = new App();
---
+++ src/js/utils/dom.js
@@ -0,0 +1,2 @@
+// HTML로 부터 태그를 가져올 때 관용적으로 '$'기호 사용
+export const $ = selector => document.querySelector(selector);

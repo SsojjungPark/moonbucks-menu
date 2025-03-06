c4d125f283a8312e6cb2a44f5ef47360e9e7dc82
Sojung Park <sojung6458@gmail.com>
2025-03-06 12:57:44 +0900

    ♻️ [REFACTOR] step3:  api 호출 객체 추상화
    - api/index.js: 리팩토링

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,69 +1,85 @@
 const BASE_URL = "http://localhost:3000";
 
-const MenuApi = {
-  // 카테고리 불러오기
-  async getAllMenuByCategory(category) {
-    const response = await fetch(`${BASE_URL}/api/category/${category}/menu`);
-    return response.json();
-  },
-
-  // 메뉴명 추가
-  async createMenu(category, name) {
-    const response = await fetch(`${BASE_URL}/api/category/${category}/menu`, {
+const HTTP_METHOD = {
+  POST(data) {
+    return {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify({ name }),
-    });
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-    }
+      body: JSON.stringify(data),
+    };
+  },
+  PUT(data) {
+    return {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: data ? JSON.stringify(data) : null,
+    };
+  },
+  DELETE() {
+    return {
+      method: "DELETE",
+    };
+  },
+};
+
+const request = async (url, option) => {
+  const response = await fetch(url, option);
+  if (!response.ok) {
+    alert("에러가 발생했습니다.");
+    console.error("에러가 발생했습니다.");
+  }
+  return response.json();
+};
+
+const requestWithoutJson = async (url, option) => {
+  const response = await fetch(url, option);
+  if (!response.ok) {
+    alert("에러가 발생했습니다.");
+    console.error("에러가 발생했습니다.");
+  }
+  return response;
+};
+
+const MenuApi = {
+  // 카테고리 불러오기
+  getAllMenuByCategory(category) {
+    return request(`${BASE_URL}/api/category/${category}/menu`);
+  },
+
+  // 메뉴명 추가
+  createMenu(category, name) {
+    return request(
+      `${BASE_URL}/api/category/${category}/menu`,
+      HTTP_METHOD.POST({ name })
+    );
   },
 
   // 메뉴명 수정
-  async updateMenu(category, name, menuId) {
-    const response = await fetch(
+  updateMenu(category, name, menuId) {
+    return request(
       `${BASE_URL}/api/category/${category}/menu/${menuId}`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({ name }),
-      }
+      HTTP_METHOD.PUT({ name })
     );
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-    }
-    return response.json();
   },
 
   // 품절 메뉴 토글
-  async toggleSoldoutMenu(category, menuId) {
-    const response = await fetch(
+  toggleSoldoutMenu(category, menuId) {
+    return request(
       `${BASE_URL}/api/category/${category}/menu/${menuId}/soldout`,
-      {
-        method: "PUT",
-      }
+      HTTP_METHOD.PUT()
     );
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-    }
-    return response.json();
   },
 
   // 메뉴 삭제
-  async deleteMenu(category, menuId) {
-    const response = await fetch(
+  deleteMenu(category, menuId) {
+    return requestWithoutJson(
       `${BASE_URL}/api/category/${category}/menu/${menuId}`,
-      {
-        method: "DELETE",
-      }
+      HTTP_METHOD.DELETE()
     );
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-    }
   },
 };
 
